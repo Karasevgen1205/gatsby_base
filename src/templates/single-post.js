74187b52e9b5f46a1bb7ce7d827950f13d22e5f0
Yauheni <yauhenikaras@ester.co>
2021-11-29 12:57:08 +0300

    add single page for each from posts

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -1,14 +1,45 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { graphql } from "gatsby"
+import {  GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const SinglePost = () => (
-  <Layout>
-    <Seo title="Post 1" />
-    <div>
-      <h1>Post 1</h1>
-    </div>
-  </Layout>
-)
+const SinglePost = ({data}) => {
+
+  const {html} = data.markdownRemark;
+  const {category, title, url, image} = data.markdownRemark.frontmatter;
+  const img = getImage(image);
+  
+  return (
+    <Layout>
+      <Seo title={title} />
+      <div>
+        <h1>{title}</h1>
+        <div>
+          <GatsbyImage image={img} alt={title} />
+        </div>
+        <div dangerouslySetInnerHTML={{__html: html}}/>
+      </div>
+    </Layout>
+  )
+}
 
 export default SinglePost
+
+export const query = graphql`
+  query PostQuery($url: String) {
+    markdownRemark(frontmatter: {url: {eq: $url}}) {
+      html
+      frontmatter {
+        category
+        title
+        url
+        image {
+          childImageSharp {
+            gatsbyImageData(width: 600)
+          }
+        }
+      }
+    }
+  }
+`
